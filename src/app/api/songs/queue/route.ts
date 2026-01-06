fd7cb3f46e78ccd7ccba737a90a3315c19f3d0d8
Rohit Yadav <rohityadav85801@gmail.com>
2026-01-06 08:34:40 +0000

    improved schema

--- src/app/api/songs/queue/route.ts
+++ src/app/api/songs/queue/route.ts
@@ -1,6 +1,10 @@
-import { NextRequest } from "next/server";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { success } from "zod";
 
-// api/songs/queue -> no in this we should get the queeu of all the song 
+// api/songs/queue -> in this we should get the queue of all the song, to show in the frontend 
 
 export async function GET(req : NextRequest){
 // now here 
@@ -8,5 +12,71 @@ export async function GET(req : NextRequest){
 // -> get the roomCode in the payload -> check room exist then 
 // -> get the roomDetils and check user is the member of the room 
 // then get the songs with for that room and 
+
+const session = await auth.api.getSession({
+    headers : await headers()
+});
+
+if(!session){
+    return NextResponse.json({
+success: false,
+message: "Anuthorized",
+    }, { status : 401})
+};
+
+const body = await req.json();
+
+if(!body.roomCode || body.roomCode.length != 8){
+    return NextResponse.json({
+        success : false,
+        message : "Not a valid roomCode"
+    }, { status : 400})
+};
+
+const roomDetails = await prisma.room.findUnique({
+    where : {
+         roomCode : body.roomCode
+    }
+});
+
+if(!roomDetails){
+    return NextResponse.json({
+     success : false,
+     message : "Room not found"     
+    }, { status : 404})
+}
+
+
+const isMember = await prisma.roomMember.findUnique({
+    where : {
+        userId_roomId : {
+            userId : session.user.id,
+            roomId : roomDetails.id
+        }
+    }
+});
+
+if(!isMember){
+    return NextResponse.json({
+  success : false,
+  message : "You are not the member of this room"
+    }, { status : 403});
+}
+
+// now here user is logeed in , and member of the room now he should 
+// able to get the queue 
+
+const queueSongs = await prisma.song.findMany({
+    where : {
+        roomId : roomDetails.id,
+    },
+    include : {
+        vote : true
+    },
+    orderBy : [
+        
+    ]
+})
+
 
 };
